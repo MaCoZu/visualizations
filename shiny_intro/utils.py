68dc8ae0e784cdd55b322b480b281e69b4630fd9
MaCoZu <marcoz@posteo.de>
2024-03-06 20:57:16 +0100

    fresh stuff

--- shiny_intro/utils.py
+++ shiny_intro/utils.py
@@ -16,7 +16,6 @@ def get_gdp_df():
     pd.options.display.float_format = '{:,.2f}'.format
 
     indicator_code = "NY.GDP.PCAP.PP.CD"
-    metadata = wb.series.metadata.get(indicator_code) # load metadata
     gdp_ppp = wb.data.DataFrame(indicator_code, 
                                 ['AFR',
                                 'MEA', 
@@ -26,7 +25,7 @@ def get_gdp_df():
                                 ],
                                 range(1990, 2024, 1)) 
 
-    
+
 
     # clean Year from 'YR2018' to '2018'
     gdp_ppp.columns = [col.replace("YR", "") for col in gdp_ppp.columns]
@@ -40,12 +39,26 @@ def get_gdp_df():
     # rename cols
     df.columns = ["Economy", "Year", "GDP (PPP) per capita"]
 
-    return df, metadata
+    return df
+
+
+
+def get_metadata():
+    indicator_code = "NY.GDP.PCAP.PP.CD"
+    metadata = wb.series.metadata.get(indicator_code) # load metadata
+    return metadata
+
+
+region_df = pd.DataFrame(wb.region.info().items)
+# rename region code to propper region name
+def get_region(code):
+    region = region_df.loc[region_df['code'] == code, 'name'].iloc[0]
+    return region
 
 
 
 def gdp_plot():
-    df, metadata = get_gdp_df()
+    df = get_gdp_df()
     ax = sns.lineplot(
         data=df.sort_values("Year"),
         x="Year",
@@ -53,43 +66,38 @@ def gdp_plot():
         hue="Economy",
         palette="bright",
     )
-    
+
     # Customize plot
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
     spine_width = 0.5
-    
+
     for spine in ax.spines.values():
         spine.set_linewidth(spine_width)
-    ax.tick_params(axis='both', width=spine_width)
+    ax.tick_params(axis="both", width=spine_width)
     for ind, label in enumerate(ax.get_xticklabels()):
         label.set_visible(ind % 5 == 0)
-        
+
+    # Get handles and labels for the legend
+    handles, labels = ax.get_legend_handles_labels()
+
+    # Replace region codes with region names in legend labels
+    for i, label in enumerate(labels):
+        region_name = get_region(label)
+        labels[i] = region_name
+
+    # Update legend with modified labels
+    ax.legend(
+        handles,
+        labels,
+        loc="best",
+        fontsize="small",
+        frameon=False,
+        fancybox=False,
+    )
+
     ax.set_title("GDP (PPP current $) per capita.")
     ax.set_xlabel("Year")
     ax.set_ylabel("")
     return ax
 
-def gdp_plot_(df):
-    fig, ax = plt.subplots(figsize=(8, 6))
-
-    # Create line plot using Seaborn
-    plot_ = sns.lineplot(
-        data=df.sort_values("Year"),
-        x="Year",
-        y="GDP (PPP) per capita",
-        hue="Economy",
-        palette="bright"
-    )
-
-    # Customize plot
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    spine_width = 0.5
-    for spine in ax.spines.values():
-        spine.set_linewidth(spine_width)
-    ax.tick_params(axis='both', width=spine_width)
-    for ind, label in enumerate(plot_.get_xticklabels()):
-        label.set_visible(ind % 5 == 0)
-
-    return fig
