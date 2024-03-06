c5df9c653036f34d677eecba7c5cafbd6c3b00aa
MaCoZu <marcoz@posteo.de>
2024-03-06 20:56:59 +0100

    new dashboard 4x4

--- shiny_intro/app.py
+++ shiny_intro/app.py
@@ -1,44 +1,48 @@
-from functools import partial
-
 import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-import wbgapi as wb
-from faicons import icon_svg as icon
-from shiny import render
-from shiny.express import input, render, ui
-from shiny.ui import page_navbar
-from shinywidgets import render_widget
-from utils import gdp_plot
+import numpy as np
+from shiny import App, Inputs, Outputs, Session, render, ui
+from utils import gdp_plot, get_metadata
 
-ui.page_opts(title="Sidebar layout", fillable=True)
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        ui.input_slider("n", "N", min=0, max=100, value=20),
+    ),
+    
+    ui.row(
+        ui.column(5, ui.output_plot("gdp1")),
+        ui.column(5, ui.output_plot("gdp2")),
+    ),
+    
+    ui.row(
+        ui.column(5, ui.output_plot("gdp3")),
+        ui.column(5, ui.output_plot("gdp4")),
+    ),
+)
 
-with ui.sidebar(open="open", bg="#f8f8f8"):
-    with ui.accordion(id="acc", open="Section A"):
-        with ui.accordion_panel("work"):
-            ui.input_select("var", "Select variable", choices=["total_bill", "tip"])
+def server(input, output, session):
+    @output
+    @render.plot(alt="GDP (PPP current $) per capita.")
+    def gdp1():
+        ax = gdp_plot()
+        return ax
+    
+    @output
+    @render.plot(alt="GDP (PPP current $) per capita.")
+    def gdp2():
+        ax = gdp_plot()
+        return ax
+    
+    @output
+    @render.plot(alt="GDP (PPP current $) per capita.")
+    def gdp3():
+        ax = gdp_plot()
+        return ax
+    
+    @output
+    @render.plot(alt="GDP (PPP current $) per capita.")
+    def gdp4():
+        ax = gdp_plot()
+        return ax
 
-        with ui.accordion_panel("households"):
-            "Section B content"
+app = App(app_ui, server)
 
-        with ui.accordion_panel("state"):
-            "Section C content"
-
-        with ui.accordion_panel("industry"):
-            "Section D content"
-
-        with ui.accordion_panel("nature"):
-            "Section E content"
-
-
-
-with ui.tooltip():
-    icon("circle-info")
-    'hello'
-    # metadata['Longdefinition']
-
-
-@render.plot(alt="GDP (PPP current $) per capita.")
-def plot():
-    ax = gdp_plot()
-    return ax
